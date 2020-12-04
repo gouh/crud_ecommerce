5ef82265a81fe892218b8ca9dd75ef957f9bab64
Hugo Hernández Valdez <hugohv10@gmail.com>
2020-12-03 20:03:22 -0600

    FEAT (:sparkles:): Creación de los archivos base para la tabla articulos y comentarios

---
+++ database/migrations/2020_12_04_013214_create_articulos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticulosTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('articulos', function (Blueprint $table) {
+			$table->id();
+			$table->string('propietario', 50);
+			$table->text('articulo');
+			$table->string('foto', 70);
+			$table->unsignedInteger('likes');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('articulos');
+	}
+}
