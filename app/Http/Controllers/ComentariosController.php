54856f6d460299ff6054317e9cf11b4f2dd181cb
Hugo Hernández Valdez <hugohv10@gmail.com>
2020-12-04 05:12:14 -0600

    DOCS(:memo:): Documentación de los comentarios para cada articulo

--- app/Http/Controllers/ComentariosController.php
+++ app/Http/Controllers/ComentariosController.php
@@ -6,24 +6,9 @@ use App\Models\Comentarios;
 use Illuminate\Http\Request;
 
 class ComentariosController extends Controller {
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index() {
-	}
 
 	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function create() {
-	}
-
-	/**
-	 * Store a newly created resource in storage.
+	 * Almacena un nuevo comentario
 	 *
 	 * @param  \Illuminate\Http\Request  $request
 	 * @return \Illuminate\Http\Response
@@ -47,35 +32,6 @@ class ComentariosController extends Controller {
 			'menssage' => 'No se pudo guardar el comentario intente de nuevo por favor.',
 			'data' => null
 		];
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  \App\Models\Comentarios  $comentarios
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show($idArticulo) {
-		return Comentarios::where(['articulos_id' => $idArticulo])->get();
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  \App\Models\Comentarios  $comentarios
-	 * @return \Illuminate\Http\Response
-	 */
-	public function edit(Comentarios $comentarios) {
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \App\Models\Comentarios  $comentarios
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(Request $request, Comentarios $comentarios) {
 	}
 
 	/**
